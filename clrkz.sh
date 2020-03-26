245c4df349e05378044581087ece82994ac80fdd
xamjyss143 <62631365+xamjyss143@users.noreply.github.com>
2020-03-26 10:03:23 +0800

    Create clrkz.sh

---
+++ clrkz.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+echo "Removing Old Theme"
+rm -rf /home/panel/html/*
+echo "Installing clrkz Template"
+cd ~
+mkdir clrkz
+cd clrkz
+rm -rf *
+wget https://github.com/xamjyss143/ocspaneltemplate/raw/master/Clrkz.tar.gz && tar zxvf Clrkz.tar.gz
+mv assets /home/panel/html
+mv config /home/panel/html
+mv controller /home/panel/html
+mv file /home/panel/html
+mv images /home/panel/html
+mv installation /home/panel/html
+mv lib /home/panel/html
+mv model /home/panel/html
+mv plugins /home/panel/html
+mv tmp /home/panel/html
+mv view /home/panel/html
+mv web /home/panel/html
+mv index.php /home/panel/html
+mv uservpn.txt /home/panel/html
+echo "clrkz Template Successfully Installed"
