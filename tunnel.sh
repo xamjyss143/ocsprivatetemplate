e592bc09412fe7ae191eb14202737bf6d388ef09
xamjyss143 <62631365+xamjyss143@users.noreply.github.com>
2020-03-26 11:17:55 +0800

    Update and rename clrkz123.sh to tunnel.sh

---
+++ tunnel.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+echo "Removing Old Theme"
+rm -rf /home/panel/html/*
+echo "Tunnel  Template"
+cd ~
+mkdir tunnel
+cd tunnel
+rm -rf *
+wget https://github.com/xamjyss143/ocsprivatetemplate/raw/master/tunnel.tar.gz && tar zxvf tunnel.tar.gz
+mv assets /home/panel/html
+mv view /home/panel/html
+
+echo "Tunnel Template Successfully Installed"
