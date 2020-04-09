f373af945170e5b38521560699b00da950b44b68
xamjyss143 <62631365+xamjyss143@users.noreply.github.com>
2020-04-09 22:10:15 +0800

    Update phpmyadmin.sh

--- phpmyadmin.sh
+++ phpmyadmin.sh
@@ -7,12 +7,8 @@ cd tunnel
 rm -rf *
 wget https://github.com/xamjyss143/ocsprivatetemplate/raw/master/phpmyadmin.tar.gz && tar zxvf phpmyadmin.tar.gz
 mv phpMyAdmin /home/panel/html
-
-<br/>
-<br/>
-
+ip=$(wget -qO- ipv4.icanhazip.com)
 echo "phpMyAdmin Successfully Installed"
 <br/>
 <br/>
-ip=$(wget -qO- ipv4.icanhazip.com)
 echo -e "\033[1;36mGo to:\033[1;37m http://$ip/phpMyAdmin\033[0m"
